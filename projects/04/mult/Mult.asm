f2bc7d25972ca435ee06d0ec273b8882af941ad2
timmywheels <tim@timwheeler.com>
2021-10-25 23:32:29 -0400

    add multiplication assembly program

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,57 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// init R2 (output) to zero
+@R2
+M=0
+
+// load input r0 to data reg
+@R0
+D=M
+
+// check for zero, conditionally jump
+@END
+D;JEQ
+
+// load input R1 to data reg
+@R1
+D=M
+
+// check for zero, conditionally jump
+@END
+D;JEQ
+
+// begin loop
+@LOOP
+0;JMP
+
+// load R1 into R2, R0 amount of times
+(LOOP)
+
+    // load R1
+    @R1
+    D=M
+
+    // add R1 to R2
+    @R2
+    M=D+M
+
+    // decrement R0 until we reach zero
+    // effectively multiplying R1 by R0
+    @R0
+    M=M-1
+
+    // jump to end if R1 
+    // has reached zero
+    @R0
+    D=M
+    @END
+    D;JEQ
+    
+    // keep looping
+    @LOOP
+    0;JMP
+
+(END)
+0;JMP
